78e96d0d25834ad0d61f59c103f3c24930c8c3b8
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-12-11 16:08:26 +0300

    adds component modal overlay

---
+++ src/components/ModalOverlay.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modal-overlay"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  display: none;
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-black-06);
+
+  &--show {
+    display: block;
+  }
+}
+</style>
