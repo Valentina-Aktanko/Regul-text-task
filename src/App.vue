f2a05215b9ef8b1312e11ecdf8c027737b69f5c3
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-12-11 11:31:34 +0300

    added presets for future components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,130 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style>
+@font-face {
+  font-family: 'Montserrat Medium';
+  src: url('~@/assets/fonts/montserrat-medium-webfont.woff2') format('woff2'),
+    url('~@/assets/fonts/montserrat-medium-webfont.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'Montserrat Regular';
+  src: url('~@/assets/fonts/montserrat-regular-webfont.woff2') format('woff2'),
+    url('~@/assets/fonts/montserrat-regular-webfont.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'montserratsemibold';
+  src: url('~@/assets/fonts/montserrat-semibold-webfont.woff2') format('woff2'),
+    url('~@/assets/fonts/montserrat-semibold-webfont.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+* {
+  --color-white: #ffffff;
+  --color-white-2: #fafafa; /* sorry for the names */
+  --color-white-3: #f3f4f6;
+  --color-extra-light-grey: #eaecf0;
+  --color-extra-light-grey-2: #eceef2;
+  --color-light-grey: #7f899e;
+  --color-grey: #50586a;
+  --color-dark-grey: #3c3c3c;
+  --color-black-06: rgba(0, 0, 0, 0.6);
+  --color-brand-green: #53c6d1;
+  --tablet-width: 560px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  min-width: 320px;
+  margin: 0;
+  padding: 0;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  font-family: 'Montserrat Regular', 'Arial', sans-serif;
+  font-style: normal;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: var(--color-dark-grey);
+}
+
+img {
+  width: 100%;
+  height: auto;
+}
+
+.visually-hidden:not(:focus):not(:active) {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
+  white-space: nowrap;
+  -webkit-clip-path: inset(100%);
+  clip-path: inset(100%);
+  clip: rect(0, 0, 0, 0);
+  overflow: hidden;
+}
+
+/* Animation */
+@-webkit-keyframes bounce {
+  0% {
+    -webkit-transform: translateY(-2000px);
+    transform: translateY(-2000px);
+  }
+  70% {
+    -webkit-transform: translateY(30px);
+    transform: translateY(30px);
+  }
+  90% {
+    -webkit-transform: translateY(-10px);
+    transform: translateY(-10px);
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+}
+@keyframes bounce {
+  0% {
+    -webkit-transform: translateY(-2000px);
+    transform: translateY(-2000px);
+  }
+  70% {
+    -webkit-transform: translateY(30px);
+    transform: translateY(30px);
+  }
+  90% {
+    -webkit-transform: translateY(-10px);
+    transform: translateY(-10px);
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+}
+
+.button {
+  padding: 8px 12px;
+
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
   text-align: center;
-  color: #2c3e50;
-}
+  color: var(--color-white);
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  background: var(--color-brand-green);
+  border: none;
+  border-radius: 6px;
 }
 </style>
---
+++ src/views/ReviewPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1 class="visually-hidden">{{ title }}</h1>
+
+    <section class="main-content">
+      <h2 class="visually-hidden">Основной контент</h2>
+      <div class="main-content__wrapper">
+        <!-- Тут будет основной контент страницы -->
+        <button class="button main-content__button" id="review-button">
+          Оставить отзыв
+        </button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Тестовое задание на позицию HTML-верстальщик',
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.main-content__wrapper {
+  width: 320px;
+  margin: 0 auto;
+  padding: 16px;
+  padding-top: 100px; /* Тут какой-то контент */
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+@media (min-width: 560px) {
+  .main-content__wrapper {
+    min-width: var(--tablet-width);
+    max-width: 1600px;
+    padding: 24px 32px;
+    padding-top: 100px; /* Тут какой-то контент */
+  }
+}
+</style>
